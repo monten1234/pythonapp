df297932176ec2cee180e81ae58a77d7457895d4
monten1234 <touch26.takehiro@icloud.com>
2023-08-22 22:14:10 +0900

    storageにファイルをアップロードするためのコードを追加

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 import streamlit as st
+from azure.storage.blob import BlobServiceClient
 
-st.title("hello")
-st.title("hello")
-st.text("hello")
+# Azure接続情報
+CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=adachitakehiro;AccountKey=adSQfl85W6VyYuuZgxJa6lV+v13GEmbPsXZ9ImS5oJ8nyOe0woYlaHEYEOtSoZtUqc9hDa0NlNlI+ASt8k+AAg==;EndpointSuffix=core.windows.net'
+CONTAINER_NAME = 'democontainer1'
+blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
+
+def save_to_blob(file):
+    blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=file.name)
+    blob_client.upload_blob(file.getvalue())
+
+st.title('Azure Blob Storage Uploader')
+
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file:
+    save_to_blob(uploaded_file)
+    st.success('File uploaded to Azure Blob Storage!')
