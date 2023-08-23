1aaf872518434ed4a723876b2db459f2033b1255
monten1234 <touch26.takehiro@icloud.com>
2023-08-23 14:28:23 +0900

    fuck

--- app.py
+++ app.py
@@ -1,44 +1,23 @@
 import streamlit as st
 from azure.storage.blob import BlobServiceClient
 
+# Azure接続情報
+CONNECTION_STRING = 'YOUR_AZURE_STORAGE_CONNECTION_STRING'
+CONTAINER_NAME = 'your-container-name'
+blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
 
-# Get Azure Storage account details from environment variables
-azure_connection_string = "DefaultEndpointsProtocol=https;AccountName=adachitakehiro;AccountKey=B3Px4aJ9dBCJJQAkCW6ZRDJFqYk7E6EYNgosHvbloU3+6QTNV2cmq/w3vFtpj0C4eMvA2VzscIv7+ASt61DFww==;EndpointSuffix=core.windows.net"
-azure_container_name = "democontainer1"
+def save_to_blob(file):
+    blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=file.name)
+    blob_client.upload_blob(file.getvalue())
 
-def main():
-    st.title("File Upload to Azure Storage")
+    # ファイルのURLを取得
+    blob_url = blob_client.url
+    return blob_url
 
-    # File upload section
-    st.subheader("Upload a file")
-    uploaded_file = st.file_uploader("Choose a file", type=["txt", "csv", "xlsx", "pdf", "docx", "doc"])
+st.title('Azure Blob Storage Uploader')
 
-    if uploaded_file is not None:
-        # Upload the file to Azure Storage and display success message
-        if upload_to_azure_storage(uploaded_file, uploaded_file.name):
-            st.success("File upload successful!")
-        else:
-            st.error("File upload failed.")
+uploaded_file = st.file_uploader("Choose a file")
 
-def upload_to_azure_storage(file, file_name):
-    try:
-        # Create a BlobServiceClient object using the connection string
-        blob_service_client = BlobServiceClient.from_connection_string(azure_connection_string)
-
-        # Create a new blob client using the container name and the original file name
-        blob_client = blob_service_client.get_blob_client(container=azure_container_name, blob=file_name)
-
-        # Upload the file contents to Azure Storage
-        blob_client.upload_blob(file, overwrite=True)
-
-        # ファイルのURLを取得
-        blob_url = blob_client.url
-        return blob_url
-
-        return True
-    except Exception as e:
-        print(e)
-        return False
-
-if __name__ == "__main__":
-    main()
+if uploaded_file:
+    url = save_to_blob(uploaded_file)
+    st.success(f'File uploaded to Azure Blob Storage! URL: {url}')
